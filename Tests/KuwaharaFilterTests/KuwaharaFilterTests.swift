016ecb76573d850dd7ce3de8c90a5d886041e4ee
lugalu <luizjaf01@gmail.com>
2024-03-08 22:17:22 -0300

    Feat: Base implementation of Kuwahara, needs a testing App now to properly evaluate the performance on real devices

--- Tests/KuwaharaFilterTests/KuwaharaFilterTests.swift
+++ Tests/KuwaharaFilterTests/KuwaharaFilterTests.swift
@@ -3,10 +3,8 @@ import XCTest
 
 final class KuwaharaFilterTests: XCTestCase {
     func testExample() throws {
-        // XCTest Documentation
-        // https://developer.apple.com/documentation/xctest
-
-        // Defining Test Cases and Test Methods
-        // https://developer.apple.com/documentation/xctest/defining_test_cases_and_test_methods
+        let image: UIImage = UIImage(named: "testImage", in: Bundle.module, with: nil)!
+        
+        XCTAssertNoThrow(try image.applyKuwahara(type: .basicKuwahara, size: 13), "It Threw!")
     }
 }
